8f2035b012b5692f4578f5da8e965b60304ee3e0
Asem130 <asemelgendi13@gmail.com>
2024-08-11 02:52:48 +0300

    finish login

---
+++ lib/routes.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerce_app/core/constant/routes.dart';
+import 'package:ecommerce_app/core/middleware/mymiddleware.dart';
+import 'package:ecommerce_app/view/screen/auth/forgetpassword/forgetpassword.dart';
+import 'package:ecommerce_app/view/screen/auth/forgetpassword/resetpassword.dart';
+import 'package:ecommerce_app/view/screen/auth/forgetpassword/success_resetpassword.dart';
+import 'package:ecommerce_app/view/screen/auth/forgetpassword/verifycode.dart';
+import 'package:ecommerce_app/view/screen/auth/login.dart';
+import 'package:ecommerce_app/view/screen/auth/signup.dart';
+import 'package:ecommerce_app/view/screen/auth/success_signup.dart';
+import 'package:ecommerce_app/view/screen/auth/verifycodesignup.dart';
+import 'package:ecommerce_app/view/screen/language.dart';
+import 'package:ecommerce_app/view/screen/onboarding.dart';
+
+import 'package:get/get.dart';
+
+List<GetPage<dynamic>>? routes = [
+  GetPage(name: "/", page: () => const Language() , middlewares: [
+    MyMiddleWare()
+  ]),
+  // GetPage(name: "/", page: () => TestView()),
+
+  GetPage(name: AppRoute.login, page: () => const Login()),
+  GetPage(name: AppRoute.signUp, page: () => const SignUp()),
+  GetPage(name: AppRoute.forgetPassword, page: () => const ForgetPassword()),
+  GetPage(name: AppRoute.verfiyCode, page: () => const VerfiyCode()),
+  GetPage(name: AppRoute.resetPassword, page: () => const ResetPassword()),
+  GetPage(name: AppRoute.successResetpassword, page: () => const SuccessResetPassword()),
+  GetPage(name: AppRoute.successSignUp, page: () => const SuccessSignUp()),
+  GetPage(name: AppRoute.onBoarding, page: () => const OnBoarding()),
+  GetPage(name: AppRoute.verfiyCodeSignUp, page: () => const VerfiyCodeSignUp()),
+];
+ 
